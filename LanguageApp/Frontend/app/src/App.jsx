26af45f88003e25ae0ec08a354f6a98e74c1a522
Nisha2105 <nishadhanda21@gmail.com>
2025-03-11 19:29:06 +0530

    changes

--- LanguageApp/Frontend/app/src/App.jsx
+++ LanguageApp/Frontend/app/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 import { NavBar } from './Components/NavBar';
 import {Banner} from './Components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- LanguageApp/Frontend/app/src/Components/Banner.jsx
+++ LanguageApp/Frontend/app/src/Components/Banner.jsx
@@ -5,16 +5,16 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
-    
+
     return(
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Webdecoded`}<span className="wrap">web developer</span></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci tempora natus tenetur consequatur ipsum ratione maxime harum aut culpa, id sunt, delectus illum dolor explicabo modi totam non facilis. Atque.</p>
-                        <button onClick={() => console.log('conect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                        <span className="tagline">Welcome to Language Learning.</span>
+                        <h1>{`Learn Languages the `}<span className="wrap">Fun Way!</span></h1>
+                        <p>Boost your fluency with interactive lessons, real-world conversations, and AI-powered feedback. Choose from multiple languages and start your journey today!</p>
+                        <button onClick={() => console.log('connect')}>Get Started <ArrowRightCircle size={25}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                     <img src={headerImg} alt="Header Img" />
